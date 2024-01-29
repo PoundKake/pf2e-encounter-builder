6f9a5bd1d7811ce75b01a9f27a7f3ca171f0c0d6
PoundKake <vance.christian@gmail.com>
2024-01-29 16:50:21 -0500

    re-added new project scaffolding and integrated vuetify where needed to use their component library instead of the default one with Vue.

--- pf2e-encounter-builder/src/components/__tests__/HelloWorld.spec.js
+++ pf2e-encounter-builder/src/components/__tests__/HelloWorld.spec.js
@@ -3,9 +3,18 @@ import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import HelloWorld from '../HelloWorld.vue'
 
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
 describe('HelloWorld', () => {
   it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' }, plugins: [vuetify] })
     expect(wrapper.text()).toContain('Hello Vitest')
   })
 })
