90c33b8cfdd66f7a4833f741e552ef88fc38b3e1
PoundKake <vance.christian@gmail.com>
2024-01-29 16:51:20 -0500

    Added encounter budget and encounter budget view base class.

---
+++ pf2e-encounter-builder/src/views/EncounterView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import EncounterBudget from '@/components/EncounterBudget.vue'
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is a basic Encounter Builder Tool for Pathfinder Second Edition</h1>
+  </div>
+  <div>
+    <EncounterBudget />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
