c5dc129826b5e7693f60470e0ce1d8b9b038e4fd
PoundKake <vance.christian@gmail.com>
2024-02-02 15:18:16 -0500

    Filled out base values for encounter budget and integrated pinia store

--- pf2e-encounter-builder/src/components/EncounterBudget.vue
+++ pf2e-encounter-builder/src/components/EncounterBudget.vue
@@ -1,12 +1,24 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
+import { useXpBudgetStore } from '@/stores/xpbudgets'
+
+const xpbudgetstore = useXpBudgetStore()
 </script>
 
 <template>
-  <div></div>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Threat</th>
+        <th class="text-left">XP Budget</th>
+        <th class="text-left">Character Adjustment</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in xpbudgetstore.defaultXpBudgetValues" :key="item.threat">
+        <td>{{ item.threat }}</td>
+        <td>{{ item.xp }}</td>
+        <td>{{ item.ca }}</td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
