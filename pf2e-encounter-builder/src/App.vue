7add88d40b61b825fe330cf932655a7ade893bd9
Christian Vance <cvance@m-apd-cvance.neb.com>
2024-02-02 10:59:48 -0500

    Fixed the broken tab issue. Now routes work with vuetify tabs.

--- pf2e-encounter-builder/src/App.vue
+++ pf2e-encounter-builder/src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue'
+
+const tab = ref(null)
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/encounter">Encounter Builder</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <v-card>
+    <v-tabs v-model="tab" color="deep-purple-accent-4" align-tabs="center">
+      <RouterLink to="/"><v-tab :value="1">Home</v-tab></RouterLink>
+      <RouterLink to="/about"><v-tab :value="2">About</v-tab></RouterLink>
+      <RouterLink to="/encounter"><v-tab :value="3">Encounter</v-tab></RouterLink>
+    </v-tabs>
+  </v-card>
   <RouterView />
 </template>
