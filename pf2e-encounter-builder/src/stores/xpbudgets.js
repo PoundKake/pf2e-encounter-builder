21e3dae81a77051d8249a995a382755549e4b9d9
PoundKake <vance.christian@gmail.com>
2024-02-02 15:19:15 -0500

    added default valued for xp budget

--- pf2e-encounter-builder/src/stores/xpbudgets.js
+++ pf2e-encounter-builder/src/stores/xpbudgets.js
@@ -1,12 +1,36 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+const defaultXpBudgetValues = [
+  {
+    threat: 'Trivial',
+    xp: '40',
+    ca: '10' //ca = Character Adjustment
+  },
+  {
+    threat: 'Low',
+    xp: '60',
+    ca: '15' //ca = Character Adjustment
+  },
+  {
+    threat: 'Moderate',
+    xp: '80',
+    ca: '20' //ca = Character Adjustment
+  },
+  {
+    threat: 'Severe',
+    xp: '120',
+    ca: '30' //ca = Character Adjustment
+  },
+  {
+    threat: 'Extreme',
+    xp: '160',
+    ca: '40' //ca = Character Adjustment
   }
+]
 
-  return { count, doubleCount, increment }
+export const useXpBudgetStore = defineStore('xpbudget', () => {
+  const xpbudget = ref(0)
+
+  return { xpbudget, defaultXpBudgetValues }
 })
