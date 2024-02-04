bd3531e847c8656ddd2c7100dd29c386f908eb19
PoundKake <vance.christian@gmail.com>
2024-02-03 22:23:56 -0500

    Added scaffolding for party level manager widget.

---
+++ pf2e-encounter-builder/src/components/PartyLevelManager.vue
@@ -0,0 +1,27 @@
+<script setup>
+const elevationValue = 2
+</script>
+<template>
+  <v-container>
+    <v-sheet :elevation="elevationValue">
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col>
+            <h1>Current Party Level</h1>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn density="compact" icon="mdi-plus"></v-btn>
+          </v-col>
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-col>
+            <p>1</p>
+          </v-col>
+          <v-col cols="auto">
+            <v-btn density="compact" icon="mdi-plus"></v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+  </v-container>
+</template>
