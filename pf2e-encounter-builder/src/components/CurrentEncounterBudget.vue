f013ce1c97ce66c3573ad4c91f1522a13a4574f7
PoundKake <vance.christian@gmail.com>
2024-02-03 22:22:23 -0500

    Added scaffolding for current encounter budget component.

---
+++ pf2e-encounter-builder/src/components/CurrentEncounterBudget.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useXpBudgetStore } from '@/stores/xpbudgets'
+// import { usePlayerCountStore } from '@/stores/playercount'
+
+const xpbudgetstore = useXpBudgetStore()
+// const playercountstore = usePlayerCountStore()
+</script>
+
+<template>
+  <v-table>
+    <thead>
+      <tr>
+        <th class="text-left">Threat</th>
+        <th class="text-left">XP Budget</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in xpbudgetstore.currentXpBudgetValues" :key="item.threat">
+        <td>{{ item.threat }}</td>
+        <td>{{ item.xp }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
