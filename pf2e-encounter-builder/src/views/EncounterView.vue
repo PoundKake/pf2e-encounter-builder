996206a01f6b10881ddca29e6c0f5695df5cf1fc
PoundKake <vance.christian@gmail.com>
2024-02-03 22:29:28 -0500

    Added grid container layout to Encounter budget view, as well as, Current Encounter Budget Component and Party Level Manager component.

--- pf2e-encounter-builder/src/views/EncounterView.vue
+++ pf2e-encounter-builder/src/views/EncounterView.vue
@@ -1,19 +1,48 @@
 <script setup>
 import EncounterBudget from '@/components/EncounterBudget.vue'
+import CurrentEncounterBudget from '@/components/CurrentEncounterBudget.vue'
+import PartyLevelManager from '@/components/PartyLevelManager.vue'
+
+const elevationValue = 2
 </script>
 
 <template>
-  <div class="container">
+  <div class="flex-class">
     <h1>This is a basic Encounter Builder Tool for Pathfinder Second Edition</h1>
   </div>
-  <div class="container">
-    <EncounterBudget />
-  </div>
+
+  <v-container>
+    <v-row no-gutters class="flex-class">
+      <v-col></v-col>
+      <v-col>
+        <v-sheet class="flex-class">
+          <PartyLevelManager />
+        </v-sheet>
+      </v-col>
+      <v-col></v-col>
+    </v-row>
+    <v-row no-gutters class="flex-class">
+      <v-col class="margin-class">
+        <v-sheet :elevation="elevationValue">
+          <CurrentEncounterBudget />
+        </v-sheet>
+      </v-col>
+      <v-col class="margin-class">
+        <v-sheet :elevation="elevationValue">
+          <EncounterBudget />
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style>
-.container {
+.flex-class {
   display: flex;
   justify-content: center;
+}
+
+.margin-class {
+  margin: 1%;
 }
 </style>
