0f661f80f36f020d0ffb80e2d42567aef54a6b0f
PoundKake <vance.christian@gmail.com>
2024-02-07 16:27:56 -0500

    Added vue component scaffolding for player counter widget.

---
+++ pf2e-encounter-builder/src/components/PlayerCountManager.vue
@@ -0,0 +1,39 @@
+<script setup>
+const elevationValue = 2
+</script>
+<template>
+  <v-container>
+    <v-sheet :elevation="elevationValue">
+      <v-container>
+        <v-row align="center" justify="center">
+          <v-col>
+            <h1 class="h1-text-class">Current Player Count</h1>
+          </v-col>
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-col align="center">
+            <v-container> {{ partylevelstore.partylevel }} </v-container>
+          </v-col>
+          <v-col>
+            <v-row>
+              <v-col align="center">
+                <v-btn density="compact" icon="mdi-plus"></v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col align="center">
+                <v-btn density="compact" icon="mdi-plus"></v-btn>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+  </v-container>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 100%;
+}
+</style>
