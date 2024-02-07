8a649002c6bcbb04f0922c5e85d0757fb086bcc8
PoundKake <vance.christian@gmail.com>
2024-02-06 22:41:08 -0500

    fixed containerization issue with Party Level Manager. Starting to build functionality in component.

--- pf2e-encounter-builder/src/components/PartyLevelManager.vue
+++ pf2e-encounter-builder/src/components/PartyLevelManager.vue
@@ -1,4 +1,7 @@
 <script setup>
+import { usePartyLevelStore } from '@/stores/partylevel'
+
+var partylevelstore = usePartyLevelStore()
 const elevationValue = 2
 </script>
 <template>
@@ -7,21 +10,33 @@ const elevationValue = 2
       <v-container>
         <v-row align="center" justify="center">
           <v-col>
-            <h1>Current Party Level</h1>
-          </v-col>
-          <v-col cols="auto">
-            <v-btn density="compact" icon="mdi-plus"></v-btn>
+            <h1 class="h1-text-class">Current Party Level</h1>
           </v-col>
         </v-row>
         <v-row align="center" justify="center">
-          <v-col>
-            <p>1</p>
+          <v-col align="center">
+            <v-container> {{ partylevelstore.partylevel }} </v-container>
           </v-col>
-          <v-col cols="auto">
-            <v-btn density="compact" icon="mdi-plus"></v-btn>
+          <v-col>
+            <v-row>
+              <v-col align="center">
+                <v-btn density="compact" icon="mdi-plus"></v-btn>
+              </v-col>
+            </v-row>
+            <v-row>
+              <v-col align="center">
+                <v-btn density="compact" icon="mdi-plus"></v-btn>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-container>
     </v-sheet>
   </v-container>
 </template>
+
+<style scoped>
+h1 {
+  font-size: 100%;
+}
+</style>
