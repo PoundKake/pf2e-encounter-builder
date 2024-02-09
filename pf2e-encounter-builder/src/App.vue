aec3b0c2d977be923c4910517b1b0d7221f4c0e4
PoundKake <vance.christian@gmail.com>
2024-02-09 14:23:58 -0500

    removed binded value of tabs as they where not needed.

--- pf2e-encounter-builder/src/App.vue
+++ pf2e-encounter-builder/src/App.vue
@@ -8,9 +8,9 @@ const tab = ref(null)
 <template>
   <v-card>
     <v-tabs v-model="tab" color="deep-purple-accent-4" align-tabs="center">
-      <RouterLink to="/"><v-tab :value="1">Home</v-tab></RouterLink>
-      <RouterLink to="/about"><v-tab :value="2">About</v-tab></RouterLink>
-      <RouterLink to="/encounter"><v-tab :value="3">Encounter</v-tab></RouterLink>
+      <RouterLink to="/"><v-tab>Home</v-tab></RouterLink>
+      <RouterLink to="/about"><v-tab>About</v-tab></RouterLink>
+      <RouterLink to="/encounter"><v-tab>Encounter</v-tab></RouterLink>
     </v-tabs>
   </v-card>
   <RouterView />
