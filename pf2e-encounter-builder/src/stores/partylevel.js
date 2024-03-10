e6992312682b8f59f14a87be3fa1d21ba9488159
PoundKake <vance.christian@gmail.com>
2024-03-10 19:14:42 -0400

    added object for keeping track of current creature xp based on party level.

--- pf2e-encounter-builder/src/stores/partylevel.js
+++ pf2e-encounter-builder/src/stores/partylevel.js
@@ -1,28 +1,64 @@
 import { defineStore } from 'pinia'
 
 export const usePartyLevelStore = defineStore('partylevel', () => {
-  const partylevel = 0
+  const currentpartylevel = 1
   const defaultPartyLevel = 1
 
-  const partylevelMinus4 = partylevel.value - 4
-  const partylevelMinus3 = partylevel.value - 3
-  const partylevelMinus2 = partylevel.value - 2
-  const partylevelMinus1 = partylevel.value - 1
-  const partylevelPlus1 = partylevel.value + 1
-  const partylevelPlus2 = partylevel.value + 2
-  const partylevelPlus3 = partylevel.value + 3
-  const partylevelPlus4 = partylevel.value + 4
+  let currentCreatureLevelXPValues = {
+    partylevelminus4: { calctext: 'Party Level - 4', level: -3, xp: 10, role: 'Low-threat lackey' },
+    partylevelminus3: {
+      calctext: 'Party Level - 3',
+      level: -2,
+      xp: 15,
+      role: 'Low- or moderate-threat lackey'
+    },
+    partylevelminus2: {
+      calctext: 'Party Level - 2',
+      level: -1,
+      xp: 20,
+      role: 'Any lackey or standard creature'
+    },
+    partylevelminus1: {
+      calctext: 'Party Level - 1',
+      level: 0,
+      xp: 30,
+      role: 'Any standard creature'
+    },
+    partylevel: {
+      calctext: 'Party Level',
+      level: 1,
+      xp: 40,
+      role: 'Any standard creature or low-threat boss'
+    },
+    partylevelplus1: {
+      calctext: 'Party Level + 1',
+      level: 2,
+      xp: 60,
+      role: 'Low- or moderate-threat boss'
+    },
+    partylevelplus2: {
+      calctext: 'Party Level + 2',
+      level: 3,
+      xp: 80,
+      role: 'Moderate- or severe-threat boss'
+    },
+    partylevelplus3: {
+      calctext: 'Party Level + 3',
+      level: 4,
+      xp: 120,
+      role: 'Severe- or extreme-threat boss'
+    },
+    partylevelplus4: {
+      calctext: 'Party Level + 4',
+      level: 5,
+      xp: 160,
+      role: 'Extreme-threat solo boss'
+    }
+  }
 
   return {
-    partylevel,
+    currentpartylevel,
     defaultPartyLevel,
-    partylevelMinus4,
-    partylevelMinus3,
-    partylevelMinus2,
-    partylevelMinus1,
-    partylevelPlus1,
-    partylevelPlus2,
-    partylevelPlus3,
-    partylevelPlus4
+    currentCreatureLevelXPValues
   }
 })
